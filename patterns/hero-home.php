c95867d9ba168b8e2a060b64f7437a94d8dc7165
ipatate <faramazpat@gmail.com>
2022-09-02 19:22:29 +0200

    start pattern for hero home page

---
+++ patterns/hero-home.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Title: Hero Home
+ * Slug: press-wind/hero-home
+ * Categories: press-wind/press-wind-patterns
+ */
+?>
+
+<!-- wp:group {"className":"gm-hero-home"} -->
+<div class="wp-block-group gm-hero-home">
+  <!-- wp:group -->
+  <div class="wp-block-group">
+    <!-- wp:image {"id":74,"sizeSlug":"large","linkDestination":"none"} -->
+    <figure class="wp-block-image size-large"><img src="http://wpperformance.localhost/wp-content/uploads/2022/09/wasa-crispbread-IgYBpcquAg4-unsplash-1024x775.jpg" alt="" class="wp-image-74" /></figure>
+    <!-- /wp:image -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group">
+    <!-- wp:paragraph -->
+    <p>totalement open-source</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:heading -->
+    <h2>Improve WordPress Performance by example</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph -->
+    <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:paragraph -->
+    <p><a href="http://wpperformance.localhost/hello-world/" data-type="post" data-id="1">Voir les derniers articles</a></p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
