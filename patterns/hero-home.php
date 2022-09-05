c82e8ca855c863deff31d54e185b9772a5401cdb
ipatate <faramazpat@gmail.com>
2022-09-05 11:01:29 +0200

    End of pattern hero home

--- patterns/hero-home.php
+++ patterns/hero-home.php
@@ -19,21 +19,61 @@
 
   <!-- wp:group -->
   <div class="wp-block-group">
-    <!-- wp:paragraph -->
-    <p>totalement open-source</p>
-    <!-- /wp:paragraph -->
+    <!-- wp:group -->
+    <div class="wp-block-group">
+      <!-- wp:paragraph -->
+      <p>totalement open-source</p>
+      <!-- /wp:paragraph -->
 
-    <!-- wp:heading -->
-    <h2>Improve WordPress Performance by example</h2>
-    <!-- /wp:heading -->
+      <!-- wp:heading -->
+      <h2>Improve WordPress Performance by example</h2>
+      <!-- /wp:heading -->
 
-    <!-- wp:paragraph -->
-    <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
-    <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
+      <!-- /wp:paragraph -->
 
-    <!-- wp:paragraph -->
-    <p><a href="http://wpperformance.localhost/hello-world/" data-type="post" data-id="1">Voir les derniers articles</a></p>
-    <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p><a href="http://wpperformance.localhost/hello-world/" data-type="post" data-id="1">Voir les derniers articles</a></p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group">
+      <!-- wp:heading {"level":3} -->
+      <h3>Retrouvez-nous sur</h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group">
+        <!-- wp:social-links -->
+        <ul class="wp-block-social-links">
+          <!-- wp:social-link {"url":"https://github.com/WP-Performance","service":"github"} /-->
+        </ul>
+        <!-- /wp:social-links -->
+
+        <!-- wp:heading {"level":4} -->
+        <h4>Github</h4>
+        <!-- /wp:heading -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group">
+        <!-- wp:social-links -->
+        <ul class="wp-block-social-links">
+          <!-- wp:social-link {"url":"https://www.youtube.com/channel/UCx8Sx_WiRkT5e14CTAhr5cA","service":"youtube"} /-->
+        </ul>
+        <!-- /wp:social-links -->
+
+        <!-- wp:heading {"level":4} -->
+        <h4>YouTube</h4>
+        <!-- /wp:heading -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 </div>
