46dbdf08bac5182c24a7b7cde883c4ae7842397d
ipatate <faramazpat@gmail.com>
2022-09-05 14:12:16 +0200

    start hero page pattern

---
+++ patterns/hero-page.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Title: Hero Page
+ * Slug: press-wind/hero-page
+ * Categories: press-wind/press-wind-patterns
+ */
+?>
+
+<!-- wp:group {"className":"gm-hero-page"} -->
+<div class="wp-block-group gm-hero-page">
+  <!-- wp:group -->
+  <div class="wp-block-group">
+    <!-- wp:image {"id":74,"sizeSlug":"large","linkDestination":"none"} -->
+    <figure class="wp-block-image size-large"><img src="/wp-content/uploads/2022/09/wasa-crispbread-IgYBpcquAg4-unsplash-1024x775.jpg" alt="" class="wp-image-74" /></figure>
+    <!-- /wp:image -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group">
+    <!-- wp:group -->
+    <div class="wp-block-group">
+
+      <!-- wp:heading -->
+      <h2>Improve WordPress Performance by example</h2>
+      <!-- /wp:heading -->
+
+    </div>
+    <!-- /wp:group -->
+
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
