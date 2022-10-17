947f9143da8588767fec813f51bedd9a6313f560
ipatate <faramazpat@gmail.com>
2022-10-17 18:54:41 +0200

    update patern

--- patterns/simple.php
+++ patterns/simple.php
@@ -7,25 +7,29 @@
  */
 ?>
 
-<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"className":"wpp-simple-bloc","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
-<div class="wp-block-group alignwide wpp-simple-bloc">
-  <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left","verticalAlignment":"top"}} -->
-  <div class="wp-block-group">
-    <!-- wp:heading {"fontSize":"3xlarge"} -->
-    <h2 class="has-3-xlarge-font-size">Thème PressWind</h2>
-    <!-- /wp:heading -->
-  </div>
-  <!-- /wp:group -->
+<!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group alignwide">
+  <!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"className":"wpp-simple-bloc","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+  <div class="wp-block-group alignwide wpp-simple-bloc">
+    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left","verticalAlignment":"top"}} -->
+    <div class="wp-block-group">
+      <!-- wp:heading {"fontSize":"4xlarge"} -->
+      <h2 class="has-4-xlarge-font-size"><a href="https://wp-performance.com/theme-presswind-un-boilerplate-base-sur-tailwindcss-et-vitejs/" data-type="page" data-id="51">PressWind, un boilerplate TailwindCSS et ViteJS</a></h2>
+      <!-- /wp:heading -->
+    </div>
+    <!-- /wp:group -->
 
-  <!-- wp:group {"layout":{"type":"constrained"}} -->
-  <div class="wp-block-group">
-    <!-- wp:paragraph {"fontSize":"base"} -->
-    <p class="has-base-font-size">Une base de thème WordPress compatible Gutenberg, basé sur Vite.JS et TailwindCSS.</p>
-    <!-- /wp:paragraph -->
+    <!-- wp:group {"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group">
+      <!-- wp:paragraph {"fontSize":"base"} -->
+      <p class="has-base-font-size">Un boilerplate compatible Gutenberg basé sur TailwindCSS et ViteJS.<br>Disponible en 2 versions. Une version classique et une version Full Site Editing.<br>Créez votre thème WordPress avec des outils modernes.</p>
+      <!-- /wp:paragraph -->
 
-    <!-- wp:paragraph {"fontSize":"base"} -->
-    <p class="has-base-font-size"><a href="/test-hero-page/" data-type="page" data-id="105">En savoir plus sur le theme PressWind</a></p>
-    <!-- /wp:paragraph -->
+      <!-- wp:paragraph {"fontSize":"base"} -->
+      <p class="has-base-font-size"><a href="/theme-presswind-un-boilerplate-base-sur-tailwindcss-et-vitejs/" data-type="page" data-id="51">En savoir plus sur le theme PressWind</a></p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 </div>
