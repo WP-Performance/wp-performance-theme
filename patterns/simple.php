5f3794ea6ea6cf53ad69226eb4d5872bc6a2b1b2
ipatate <faramazpat@gmail.com>
2022-10-17 18:21:05 +0200

    add pattern for home page

---
+++ patterns/simple.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Title: Simple
+ * Slug: press-wind/simple
+ * Categories: press-wind/press-wind-patterns
+ */
+?>
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"className":"wpp-simple-bloc","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group alignwide wpp-simple-bloc">
+  <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left","verticalAlignment":"top"}} -->
+  <div class="wp-block-group">
+    <!-- wp:heading {"fontSize":"3xlarge"} -->
+    <h2 class="has-3-xlarge-font-size">Thème PressWind</h2>
+    <!-- /wp:heading -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group {"layout":{"type":"constrained"}} -->
+  <div class="wp-block-group">
+    <!-- wp:paragraph {"fontSize":"base"} -->
+    <p class="has-base-font-size">Une base de thème WordPress compatible Gutenberg, basé sur Vite.JS et TailwindCSS.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:paragraph {"fontSize":"base"} -->
+    <p class="has-base-font-size"><a href="/test-hero-page/" data-type="page" data-id="105">En savoir plus sur le theme PressWind</a></p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
