92758dce10df89ac30955bd22efdb6a6d65f1841
ipatate <faramazpat@gmail.com>
2022-10-26 10:01:02 +0200

    add snippet section

--- functions.php
+++ functions.php
@@ -6,9 +6,13 @@ if (!defined('WP_ENV')) {
   define('WP_ENV', 'development');
 }
 
+// settings global
 require_once dirname(__FILE__) . '/inc/assets.php';
 require_once dirname(__FILE__) . '/inc/disable.php';
 require_once dirname(__FILE__) . '/inc/gutenberg/index.php';
+
+// post type
+require_once dirname(__FILE__) . '/post-type/snippet.php';
 
 // pwa icons
 if (file_exists(dirname(__FILE__) . '/inc/pwa_head.php')) {
@@ -25,29 +29,6 @@ function setup()
   add_theme_support('title-tag');
 
   add_theme_support('post-thumbnails');
-
-  // add_theme_support('html5', [
-  //   'comment-form',
-  //   'comment-list',
-  //   'gallery',
-  //   'caption',
-  // ]);
-
-  // add_theme_support('post-formats', [
-  //   'aside',
-  //   'image',
-  //   'video',
-  //   'quote',
-  //   'link',
-  //   'gallery',
-  //   'audio',
-  // ]);
-
-  // yes we can use register menu with FSE :)
-  // register_nav_menus(array(
-  //   'primary'   => __('Primary Menu', 'press-wind'),
-  //   // 'secondary' => __('Secondary Menu', 'press-wind')
-  // ));
 
 
   load_theme_textdomain('press-wind', get_template_directory() . '/languages');
