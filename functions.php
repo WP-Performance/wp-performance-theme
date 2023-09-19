4440f423deb59ba6afb910b5ec229faeaef5b72b
ipatate <faramazpat@gmail.com>
2023-09-19 12:27:53 +0200

    test img size

--- functions.php
+++ functions.php
@@ -90,3 +90,23 @@ register_block_style(
     'label'        => __('Text Effect', 'press-wind'),
   )
 );
+
+function add_img_size($content){
+  $pattern = '/<img [^>]*?src="(https?:\/\/[^"]+?)"[^>]*?>/iu';
+  preg_match_all($pattern, $content, $imgs);
+  foreach ( $imgs[0] as $i => $img ) {
+    if ( str_contains( $img, 'width=' ) && str_contains( $img, 'height=' ) ) {
+      continue;
+    }
+    $img_url = $imgs[1][$i];
+    $img_size = @getimagesize( $img_url );
+
+    if ( false === $img_size ) {
+      continue;
+    }
+    $replaced_img = str_replace( '<img ', '<img ' . $img_size[3] . ' ', $imgs[0][$i] );
+    $content = str_replace( $img, $replaced_img, $content );
+  }
+  return $content;
+}
+add_filter('the_content',__NAMESPACE__ . '\add_img_size');
