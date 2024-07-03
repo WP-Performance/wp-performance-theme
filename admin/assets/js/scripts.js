abbef1a98aa9ca76a2c12dc54cca92d4adfefaed
ipatate <faramazpat@gmail.com>
2024-07-03 17:43:47 +0200

    active grid for group

--- admin/assets/js/scripts.js
+++ admin/assets/js/scripts.js
@@ -1,11 +1,25 @@
 wp.domReady(() => {
-  // unregister round button style
-  // wp.blocks.unregisterBlockStyle('core/button', 'rounded')
+	// unregister round button style
+	// wp.blocks.unregisterBlockStyle('core/button', 'rounded')
 
-  // sometime unregister don't work without that
-  window._wpLoadBlockEditor.then(() => {
-    console.log('Gutenberg ready !')
+	// sometime unregister don't work without that
+	window._wpLoadBlockEditor.then(() => {
+		console.log("Gutenberg ready !");
 
-    wp.blocks.unregisterBlockStyle('core/image', ['default', 'rounded'])
-  })
-})
+		wp.blocks.unregisterBlockStyle("core/image", ["default", "rounded"]);
+	});
+
+	wp.blocks.registerBlockVariation("core/group", {
+		name: "group-grid",
+		title: wp.i18n.__("Grid", "themeslug"),
+		icon: "grid-view",
+		description: wp.i18n.__("Arrange blocks in a grid.", "themeslug"),
+		attributes: {
+			layout: {
+				type: "grid",
+			},
+		},
+		scope: ["block", "inserter", "transform"],
+		isActive: (blockAttributes) => blockAttributes.layout?.type === "grid",
+	});
+});
