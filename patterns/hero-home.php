739d358b8bc14f5bbee5d06a0e4b778cc20fc2ba
ipatate <faramazpat@gmail.com>
2024-08-19 17:33:16 +0200

    update templates

--- patterns/hero-home.php
+++ patterns/hero-home.php
@@ -7,74 +7,62 @@
  */
 ?>
 
-<!-- wp:group {"className":"gm-hero-home", "align":"wide"} -->
-<div class="wp-block-group gm-hero-home alignwide">
-  <!-- wp:group -->
-  <div class="wp-block-group">
-    <!-- wp:image {"id":74,"sizeSlug":"large","linkDestination":"none","className":"nolazy"} -->
-    <figure class="wp-block-image size-large nolazy"><img src="/wp-content/uploads/2022/09/wasa-crispbread-IgYBpcquAg4-unsplash-1024x775.jpg" alt="" class="wp-image-74" /></figure>
-    <!-- /wp:image -->
-  </div>
-  <!-- /wp:group -->
+<!-- wp:group {"align":"full","className":"gm-hero-home"} -->
+<div class="wp-block-group alignfull gm-hero-home"><!-- wp:group {"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+        <div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+            <div class="wp-block-group"><!-- wp:paragraph -->
+                <p>Totalement open-source</p>
+                <!-- /wp:paragraph -->
 
-  <!-- wp:group -->
-  <div class="wp-block-group">
-    <!-- wp:group -->
-    <div class="wp-block-group">
-      <!-- wp:paragraph -->
-      <p>totalement open-source</p>
-      <!-- /wp:paragraph -->
+                <!-- wp:heading {"textAlign":"left"} -->
+                <h2 class="wp-block-heading has-text-align-left">Améliorer la performance des sites WordPress</h2>
+                <!-- /wp:heading -->
 
-      <!-- wp:heading -->
-      <h2>Improve WordPress Performance by example</h2>
-      <!-- /wp:heading -->
+                <!-- wp:paragraph {"align":"left"} -->
+                <p class="has-text-align-left">WP Performance est un regroupement de bonnes pratiques pour développer des sites WordPress performants et maintenables sur le long terme.</p>
+                <!-- /wp:paragraph -->
 
-      <!-- wp:paragraph -->
-      <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
-      <!-- /wp:paragraph -->
+                <!-- wp:spacer {"height":"37px","style":{"layout":{"flexSize":"37px","selfStretch":"fixed"}}} -->
+                <div style="height:37px" aria-hidden="true" class="wp-block-spacer"></div>
+                <!-- /wp:spacer -->
 
-      <!-- wp:paragraph -->
-      <p><a href="/hello-world/" data-type="post" data-id="1">Voir les derniers articles</a></p>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
+                <!-- wp:paragraph {"align":"left"} -->
+                <p class="has-text-align-left"><a href="https://wp-performance.ddev.site/les-articles/" data-type="page" data-id="108">Voir les derniers articles</a></p>
+                <!-- /wp:paragraph -->
 
-    <!-- wp:group -->
-    <div class="wp-block-group">
-      <!-- wp:heading {"level":3} -->
-      <h3>Retrouvez-nous sur</h3>
-      <!-- /wp:heading -->
+                <!-- wp:spacer {"height":"37px","style":{"layout":{"flexSize":"37px","selfStretch":"fixed"}}} -->
+                <div style="height:37px" aria-hidden="true" class="wp-block-spacer"></div>
+                <!-- /wp:spacer --></div>
+            <!-- /wp:group -->
 
-      <!-- wp:group -->
-      <div class="wp-block-group">
-        <!-- wp:social-links -->
-        <ul class="wp-block-social-links">
-          <!-- wp:social-link {"url":"https://github.com/WP-Performance","service":"github"} /-->
-        </ul>
-        <!-- /wp:social-links -->
+            <!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|60"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+            <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:heading {"textAlign":"left","level":3,"fontSize":"1xlarge"} -->
+                <h3 class="wp-block-heading has-text-align-left has-1-xlarge-font-size">Retrouvez-nous sur</h3>
+                <!-- /wp:heading -->
 
-        <!-- wp:heading {"level":4} -->
-        <h4>Github</h4>
-        <!-- /wp:heading -->
-      </div>
-      <!-- /wp:group -->
+                <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+                <div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+                    <div class="wp-block-group"><!-- wp:social-links -->
+                        <ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://github.com/WP-Performance","service":"github"} /--></ul>
+                        <!-- /wp:social-links -->
 
-      <!-- wp:group -->
-      <div class="wp-block-group">
-        <!-- wp:social-links -->
-        <ul class="wp-block-social-links">
-          <!-- wp:social-link {"url":"https://www.youtube.com/channel/UCx8Sx_WiRkT5e14CTAhr5cA","service":"youtube"} /-->
-        </ul>
-        <!-- /wp:social-links -->
+                        <!-- wp:heading {"level":4,"fontSize":"xlarge"} -->
+                        <h4 class="wp-block-heading has-xlarge-font-size">Github</h4>
+                        <!-- /wp:heading --></div>
+                    <!-- /wp:group -->
 
-        <!-- wp:heading {"level":4} -->
-        <h4>YouTube</h4>
-        <!-- /wp:heading -->
-      </div>
-      <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
-  </div>
-  <!-- /wp:group -->
-</div>
+                    <!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+                    <div class="wp-block-group"><!-- wp:social-links -->
+                        <ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://www.youtube.com/channel/UCx8Sx_WiRkT5e14CTAhr5cA","service":"youtube"} /--></ul>
+                        <!-- /wp:social-links -->
+
+                        <!-- wp:heading {"level":4,"fontSize":"xlarge"} -->
+                        <h4 class="wp-block-heading has-xlarge-font-size">YouTube</h4>
+                        <!-- /wp:heading --></div>
+                    <!-- /wp:group --></div>
+                <!-- /wp:group --></div>
+            <!-- /wp:group --></div>
+        <!-- /wp:group --></div>
+    <!-- /wp:group --></div>
 <!-- /wp:group -->
