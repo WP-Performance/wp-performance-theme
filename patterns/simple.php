8fe04a001d4284203d158a9f5f5fe5043a6eca89
ipatate <faramazpat@gmail.com>
2024-08-30 18:44:42 +0200

    refactor simple pattern

--- patterns/simple.php
+++ patterns/simple.php
@@ -7,25 +7,25 @@
  */
 ?>
 
-<!-- wp:group {"align":"wide","className":"wpp-simple-bloc","style":{"spacing":{"blockGap":"var:preset|spacing|40","margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|20","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20"}}},"backgroundColor":"background","borderColor":"primary","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center"}} -->
-<div class="wp-block-group alignwide wpp-simple-bloc has-border-color has-primary-border-color has-background-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-group" style="padding-right:0;padding-left:0"><!-- wp:heading {"textAlign":"left","className":"is-style-text-effect","fontSize":"5xlarge"} -->
-        <h2 class="wp-block-heading has-text-align-left is-style-text-effect has-5-xlarge-font-size"><a href="https://wp-performance.ddev.site/theme-presswind-un-boilerplate-base-sur-tailwindcss-et-vitejs/" data-type="page" data-id="51">PressWind</a></h2>
+<!-- wp:group {"align":"wide","className":"wpp-simple-bloc","style":{"spacing":{"blockGap":"var:preset|spacing|40","margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|20","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20"}}},"backgroundColor":"background","borderColor":"primary","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide wpp-simple-bloc has-border-color has-primary-border-color has-background-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"default"}} -->
+    <div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"textAlign":"left","align":"full","className":"is-style-text-effect","fontSize":"5xlarge"} -->
+        <h2 class="wp-block-heading alignfull has-text-align-left is-style-text-effect has-5-xlarge-font-size"><a href="https://www.wp-performance.com/theme-presswind-un-boilerplate-base-sur-tailwindcss-et-vitejs/" data-type="page" data-id="51">PressWind</a></h2>
         <!-- /wp:heading -->
 
-        <!-- wp:heading {"level":3,"className":"is-style-title-hero"} -->
-        <h3 class="wp-block-heading is-style-title-hero">Un starter thème TailwindCSS et ViteJS</h3>
+        <!-- wp:heading {"level":3,"align":"full","className":"is-style-title-hero"} -->
+        <h3 class="wp-block-heading alignfull is-style-title-hero">Un starter thème TailwindCSS et ViteJS</h3>
         <!-- /wp:heading -->
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"constrained"}} -->
+    <!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"default"}} -->
     <div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"fontSize":"base"} -->
         <p class="has-base-font-size">Un starter theme compatible Gutenberg basé sur TailwindCSS et ViteJS.<br>Disponible en 2 versions. Une version classique et une version Full Site Editing.<br>Créez votre thème WordPress avec des outils modernes.</p>
         <!-- /wp:paragraph -->
 
         <!-- wp:paragraph {"fontSize":"base"} -->
-        <p class="has-base-font-size"><a href="https://wp-performance.ddev.site/theme-presswind-un-boilerplate-base-sur-tailwindcss-et-vitejs/" data-type="page" data-id="51">En savoir plus sur le starter theme PressWind</a></p>
+        <p class="has-base-font-size"><a href="https://www.wp-performance.com/theme-presswind-un-boilerplate-base-sur-tailwindcss-et-vitejs/" data-type="page" data-id="51">En savoir plus sur le starter theme PressWind</a></p>
         <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
