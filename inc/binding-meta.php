e3ccb55af2b7b2a5f5383378b0c1cc29ba612b1c
ipatate <faramazpat@gmail.com>
2024-09-01 09:40:10 +0200

    add binding for archive pages

--- inc/binding-meta.php
+++ inc/binding-meta.php
@@ -2,6 +2,29 @@
 
 namespace WP_Performance\Inc;
 
+
+function get_hero_category($args)
+{
+    global $post;
+
+    $post_type = $post->post_type;
+
+    switch ($post_type) {
+        case 'post':
+            return __('Articles, archives et categories', 'press-wind');
+            break;
+        case 'snippet':
+            return __('Snippets, archives et categories', 'press-wind');
+            break;
+        default:
+            return __('Archives et categories', 'press-wind');
+            break;
+    }
+}
+
+
+
+
 function register_block_bindings()
 {
     register_meta(
@@ -15,6 +38,11 @@ function register_block_bindings()
             'default'           => 'Totalement open-source',
         )
     );
+
+    register_block_bindings_source('wpperformance/hero-category', array(
+        'label'              => __('Hero theme category', 'press-wind'),
+        'get_value_callback' =>  __NAMESPACE__ . '\get_hero_category',
+    ));
 }
 
 add_action('init', __NAMESPACE__ . '\register_block_bindings');
